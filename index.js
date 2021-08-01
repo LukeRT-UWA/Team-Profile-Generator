e48cf8d8bdb9266f2e82ed103e1f06bf2cdf858b
Luke Thompson <16175583curtin@gmail.com>
2021-08-01 18:52:14 +0800

    base question logic created

--- index.js
+++ index.js
@@ -5,40 +5,72 @@ const inquirer = require('inquirer');
 //Require Jest?
 
 //Require markdown file
-const markdown = require('./lib/markdown')
-
-inquirer
-    .prompt([
+const markdown = require('./lib/markdown');
+const employees = [];
+function askForEmployee(type = "Manager") {
+    const baseQuestions = [
         {
-            type: 'input',
-            message: 'What is your team name?',
-            name: 'teamName',
+            type: "input",
+            message: `What is this ${type}'s name?`,
+            name: "name",
+
         },
         {
-            type: 'input',
-            message: `What is this team member's name?`,
-            name: 'memberName',
+            type: "input",
+            message: `What is this ${type}'s ID number?`,
+            name: "id",
+
         },
         {
-            type: 'list',
-            message: 'What role is this team member?',
-            name: 'memberRole',
-            choices: [
-                'Manager',
-                'Engineer',
-                'Intern',
+            type: "input",
+            message: `What is this ${type}'s E-mail address?`,
+            name: "email",
 
-            ]
         },
-    ])
-    .then((response) => {
 
+    ];
 
-        let pageContent = markdown.generatePage(response)
+    switch (type) {
+        case "Manager":
+            baseQuestions.push({
+                type: "input",
+                message: `What is this ${type}'s Office Number?`,
+                name: "officeNo",
 
-        //Create readme file
-        fs.writeFile('index.html', pageContent, (err) =>
-            err ? console.error(err) : console.log('Success!')
-        )
+            })
+            break;
+        case "Engineer":
+            baseQuestions.push({
+                type: "input",
+                message: `What is this ${type}'s Github name?`,
+                name: "github",
+
+            })
+            break;
+        case "Intern":
+            baseQuestions.push({
+                type: "input",
+                message: `What is this ${type}'s school name?`,
+                name: "school",
+
+            })
+            break;
+        default:
+            throw new Error("Failed no type provided")
     }
-    );
+
+    return inquirer.prompt(baseQuestions)
+
+}
+
+function main() {
+    return askForEmployee("Manager")
+        .then((answers) => {
+            employees.push(new Manager(...Object.values(answers)));
+        })
+        .then()
+        .catch((err) => {
+            console.log(err);
+        });
+}
+main();
